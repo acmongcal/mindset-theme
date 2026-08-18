d19dad371636d111dce71f02366e41a7fc86bbfb
Alan Christopher Mongcal <aranmongcal@gmail.com>
2026-08-18 09:43:46 -0700

    Added minor change to companu-email edit.js

--- mindset-blocks/build/company-email/index.asset.php
+++ mindset-blocks/build/company-email/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-i18n'), 'version' => 'e0316b27602af9e4686a');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-i18n'), 'version' => '62b61488520c03e25645');
